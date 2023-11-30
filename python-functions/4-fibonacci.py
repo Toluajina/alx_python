fa68efc53026a86d4efd97d5475db7959203c238
Toluajina <Toluajina>
2023-11-30 13:48:24 +0100

    python coding - functions

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -2,7 +2,11 @@ def fibonacci_sequence(n):
     if n <= 0:
         return []
 
-    sequence = [0, 1]
+    sequence = [0]
+    
+    if n > 1:
+        sequence.append(1)
+
     while len(sequence) < n:
         next_number = sequence[-1] + sequence[-2]
         sequence.append(next_number)
