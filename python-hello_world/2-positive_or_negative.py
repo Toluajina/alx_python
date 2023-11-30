245a521d2c2231a99930491c4eeb78df8ca3f22f
Toluajina <Toluajina>
2023-11-30 10:16:57 +0100

    my first python file

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-print("The number is", number)
-print("if the number is greater than 0: is positive") 
-print("if the number is 0: is zero") 
-print("if the number is less than 0: is negative")
+print(number, end=' ')
+if number > 0:
+    print("is positive")
+elif number == 0:
+    print("is zero")
+else:
+    print("is negative")
