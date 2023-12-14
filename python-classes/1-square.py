721ca6583a6d8b228a29c39d83c220a907f1ca4c
Toluajina <Toluajina>
2023-12-14 22:57:30 +0100

    Size validation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,18 @@
 class Square:
     def __init__(self, size=0):
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        
         self.__size = size
+
+    def get_size(self):
+        
+        return self.__size
+
         
