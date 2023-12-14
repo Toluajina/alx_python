e0cfcb8a15bb0b5622e923e20f2de13286ef8208
Toluajina <Toluajina>
2023-12-15 00:59:28 +0100

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -9,3 +9,13 @@ my_square = Square(3)
 # Print the type and dictionary representation of the square
 print(type(my_square))
 print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
