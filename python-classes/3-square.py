533d908fedb81436d50552bed15cf957c591fdfe
Toluajina <Toluajina>
2023-12-14 23:41:18 +0100

    Access and update private attribute

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -55,11 +55,18 @@ class Square:
         return self.__size ** 2
 
 # Example usage:
-my_square = Square(5)
-print(my_square.size)  # Accessing size using the property getter
-print(my_square.area())  # Calculating and printing the area
+my_square_1 = Square(3)
+print("Area: {}".format(my_square_1.area()))
 
-# Setting a new size using the property setter
-my_square.size = 3
-print(my_square.size)
-print(my_square.area())
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+my_square_2 = Square(5)
+print("Area: {}".format(my_square_2.area()))
