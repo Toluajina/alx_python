ed223b643f6a70ca41e9b53a891ae33dd37f48d8
Toluajina <Toluajina>
2023-12-15 00:13:53 +0100

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,27 +1,8 @@
-class Square:
-    def __init__(self, size):
-        self.__size = size
+Square = __import__('0-square').Square
 
-    def get_size(self):
-        return self.__size
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
 
-    def set_size(self, size):
-        self.__size = size
-
-    def area(self):
-        return self.__size ** 2
-
-
-# Example usage:
-# Instantiate a square with size 5
-my_square = Square(5)
-
-# Get the size of the square
-print("Size of the square:", my_square.get_size())
-
-# Set a new size for the square
-my_square.set_size(7)
-print("New size of the square:", my_square.get_size())
-
-# Calculate and print the area of the square
-print("Area of the square:", my_square.area())
+class Square(object):
+    pass
