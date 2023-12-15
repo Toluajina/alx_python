497a3fab47bde8e830f8e994a806cfe81aa50b2f
Toluajina <Toluajina>
2023-12-15 01:28:06 +0100

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -9,7 +9,7 @@ class Square(object):
         self.__size = size
 
 # Example usage:
-# Instantiate a square with size 5
+# Instantiate a square with size 3
 my_square = Square(3)
 
 # Attempting to access the size directly will result in an AttributeError
@@ -18,6 +18,13 @@ my_square = Square(3)
 # Access the size using a getter method (optional)
 def get_size(self):
     return self.__size
+
+# Print the type and dictionary representation of the square
+print(type(my_square))
+print(my_square.__dict__)
+
+# Instantiate a square with size 89
+my_square = Square(89)
 
 # Print the type and dictionary representation of the square
 print(type(my_square))
