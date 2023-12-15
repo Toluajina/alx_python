cacdea444c60db81d6923de2d81dae319b8848f2
Toluajina <Toluajina>
2023-12-15 01:04:08 +0100

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,6 +2,9 @@ class Square:
     def __init__(self, size):
         self.__size = size
 
+    def get_size(self):
+        return self.__size
+
 # Example usage:
 # Instantiate a square with size 3
 my_square = Square(3)
