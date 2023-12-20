724d87da793ee8922db8829fed2112c022a3f270
Toluajina <Toluajina>
2023-12-20 13:40:44 +0100

    Printing a square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,41 +1,37 @@
+"""Define a class Square."""
+
 class Square:
-    def __init__(self, size=0):
-        self.__size = size
+    """Represent a square."""
 
-    def area(self):
-        return self.__size**2
+    def __init__(self, size):
+        """Initialize a new square.
 
-    def my_print(self):
-        if self.__size == 0:
-            print()
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end="")
-            print()
+        Args:
+            size (int): The size of the new square.
+        """
+        self.size = size
 
     @property
     def size(self):
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
-    def size(self, size):
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = size
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
-        my_square = Square(3)
-my_square.my_print()
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
 
-print("--")
-
-my_square.size = 10
-my_square.my_print()
-
-print("--")
-
-my_square.size = 0
-my_square.my_print()
-
-print("--")
+    def my_print(self):
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
