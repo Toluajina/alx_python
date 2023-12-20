10ac9de6af47bd071ceea44c07f77e5da7421bed
Toluajina <Toluajina>
2023-12-20 08:22:35 +0100

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,22 +1,7 @@
 class Square:
     """Represent a square."""
+    pass
 
-    def __init__(self, size=0):
-        """Initialize a new Square.
-
-        Args:
-            size (int): The size of the new square.
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
-
-        my_square = Square(3)
-
-def get_size(self):
-    return self.__size
-
+my_square = Square()
 print(type(my_square))
 print(my_square.__dict__)
