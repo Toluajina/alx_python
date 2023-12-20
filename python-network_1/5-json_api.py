55e8bb486ab77d2dbbd56c95389ed1077719889c
Toluajina <Toluajina>
2023-12-20 17:57:22 +0100

    Search API

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,22 +1,23 @@
-import requests
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+
+Usage: ./8-json_api.py <letter>
+  - The letter is sent as the value of the variable `q`.
+  - If no letter is provided, sends `q=""`.
+"""
 import sys
+import requests
 
-if len(sys.argv) != 2:
-    print("Usage: python script.py <URL>")
-    sys.exit(1)
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
-# Add the 'http://' schema if missing
-if not url.startswith('http://') and not url.startswith('https://'):
-    url = 'http://' + url
-
-# Send a request to the URL
-response = requests.get(url)
-
-# Display the body of the response
-print(response.text)
-
-# Check if the HTTP status code is greater than or equal to 400
-if response.status_code >= 400:
-    print("Error code:", response.status_code)
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
