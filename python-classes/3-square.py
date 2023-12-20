467cadf2cee6313c1451635ee0051f6f82645f2c
Toluajina <Toluajina>
2023-12-20 13:39:04 +0100

    Access and update private attribute

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,72 +1,29 @@
-class Square:
-    """
-    A class that defines a square.
+"""Define a class Square."""
 
-    Attributes:
-    - __size (int): Private attribute representing the size of the square.
-    """
+class Square:
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """
-        Initializes a new instance of the Square class.
+        """Initialize a new square.
 
-        Parameters:
-        - size (int, optional): The size of the square. Defaults to 0.
+        Args:
+            size (int): The size of the new square.
         """
         self.size = size
 
     @property
     def size(self):
-        """
-        Retrieve the size of the square.
-
-        Returns:
-        - int: The size of the square.
-        """
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        """
-        Set the size of the square.
-
-        Parameters:
-        - value (int): The new size of the square.
-
-        Raises:
-        - TypeError: If value is not an integer.
-        - ValueError: If value is less than 0.
-        """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = value
 
     def area(self):
-        """
-        Calculates and returns the current square area.
-
-        Returns:
-        - int: The area of the square.
-        """
-        return self.__size ** 2
-
-# Example usage:
-my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
-
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
