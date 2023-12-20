505d449d3bb024fcc893453b3b4b15a7c0a1dfd6
Toluajina <Toluajina>
2023-12-20 13:11:49 +0100

    Area of a square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,4 +1,15 @@
 class Square:
+    """
+    A class that defines a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a new instance of the Square class with an optional size.
+        area(self): Calculates and returns the area of the square.
+    """
+
     def __init__(self, size=0):
         """
         Initializes a new instance of the Square class with an optional size.
@@ -45,4 +56,3 @@ class Square:
             int: The area of the square.
         """
         return self.__size ** 2
-    
