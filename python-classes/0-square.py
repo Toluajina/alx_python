9d4f63d5a1bb1e10c6ff11d81a59348b3210ca74
Toluajina <Toluajina>
2023-12-20 08:48:01 +0100

    Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,5 @@
 class Square(object):
-    def __init__(self, size):
+    def __init__(self, size=0):
         """
         Initializes a new instance of the Square class with the given size.
 
@@ -10,7 +10,7 @@ class Square(object):
 
 # Example usage:
 # Instantiate a square with size 3
-mysquare = Square(3)
+mysquare = Square()
 
 # Print the type and dictionary representation of the square
 print(mysquare.__dict__)
