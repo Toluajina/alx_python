eff45ef1dbed2b7439bc49d6d8095a6df7307d56
Toluajina <Toluajina>
2023-12-20 14:32:13 +0100

    Improve Geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -13,3 +13,7 @@ class BaseGeometry:
         Raises an Exception with the message 'area() is not implemented'.
         """
         raise Exception('area() is not implemented')
+    
+    # Check if 'area' is in the attributes of the class
+class_attributes = dir(BaseGeometry)
+print(class_attributes)
