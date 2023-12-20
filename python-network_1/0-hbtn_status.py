6bc14422010744f5a6c4dcfc3f14d8f32d3091aa
Toluajina <Toluajina>
2023-12-20 17:29:47 +0100

    What's my status? #1

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,8 @@
-"""Fetches https://intranet.hbtn.io/status."""
-import urllib.request
+import requests
 
+url = 'https://alu-intranet.hbtn.io/status'
+response = requests.get(url)
 
-if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+print("Body response:")
+print("\t- type: {}".format(type(response.text)))
+print("\t- content: {}".format(response.text))
