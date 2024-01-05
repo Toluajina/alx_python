10e5a5012577523884abf6ed6701fbb97f50b1e7
Toluajina <Toluajina>
2024-01-05 17:13:05 +0100

    Geometry module

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,8 +1,17 @@
-"""Defines a base geometry class BaseGeometry."""
+class MetaClass(type):
+    """
+    Metaclass to override dir() method and exclude __init_subclass__
+    """
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
-class BaseGeometry:
-    """Represent base geometry."""
+class BaseGeometry(metaclass=MetaClass):
+    """
+    BaseGeometry class that uses the overridden dir() method
+    """
+    def __dir__(self):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
     def area(self):
         """Not implemented."""
-        raise Exception("area() is not implemented")
+        raise NotImplementedError("area() is not implemented")
