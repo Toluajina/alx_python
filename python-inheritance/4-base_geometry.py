f32146246864bc45d95201ff0e005a34669b0d96
Toluajina <Toluajina>
2024-01-18 22:50:30 +0100

    Improve Geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -4,20 +4,4 @@
 
 class BaseGeometry:
     def area(self):
-        """Raises an exception with the message 'area() is not implemented.'"""
-        raise Exception("area() is not implemented")
-
-# Test the implementation
-try:
-    # Instantiate an object of BaseGeometry
-    geometry = BaseGeometry()
-
-    # Call the area() method, which will raise an exception
-    geometry.area()
-
-except Exception as e:
-    # Print the expected output
-    print(f"[Exception] {e}")
-
-# Print the attributes of the object (as per the test case)
-print(dir(geometry))
+        raise Exception('area() is not implemented')
