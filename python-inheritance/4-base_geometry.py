e504cca262a987433ce6a2deee8f65cd23e3b43a
Toluajina <Toluajina>
2024-01-18 22:37:18 +0100

    Improve Geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,18 +1,10 @@
-# 4-inherits_from.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines an inherited class-checking function."""
+# 4-base_geometry.py
+"""Defines a base geometry class BaseGeometry."""
 
 
-def inherits_from(obj, a_class):
-    """Checks if an object is an inherited instance of a class.
+class BaseGeometry:
+    """Represent base geometry."""
 
-    Args:
-        obj (any): The object to check.
-        a_class (type): The class to match the type of obj to.
-    Returns:
-        If obj is an inherited instance of a_class - True.
-        Otherwise - False.
-    """
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
-    return False
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")
