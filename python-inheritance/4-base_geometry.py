9793a115946e7446482d384c54cd6f1957cc13b8
Toluajina <Toluajina>
2024-01-18 22:53:57 +0100

    Improve Geometry

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,7 +1,9 @@
-# 4-base_geometry.py
 """Defines a base geometry class BaseGeometry."""
 
 
 class BaseGeometry:
+    """Represent base geometry."""
+
     def area(self):
-        raise Exception('area() is not implemented')
+        """Not implemented."""
+        raise Exception("area() is not implemented")
