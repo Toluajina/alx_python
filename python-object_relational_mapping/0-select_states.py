71d8a6b2c50afbdd51704e0f7f54cfc67f3d96e0
Toluajina <Toluajina>
2024-01-18 16:26:08 +0100

    Get all states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+""" module """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db_conf = {
+        'host':     'localhost',
+        'user':     sys.argv[1],
+        'passwd':   sys.argv[2],
+        'db':       sys.argv[3],
+        'port':     3306
+    }
+    db = MySQLdb.connect(**db_conf)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id")
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
