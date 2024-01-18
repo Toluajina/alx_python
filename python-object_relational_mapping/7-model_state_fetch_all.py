35f2b65ddba8fa8bdad11ec3e2ad308ea6309c93
Toluajina <Toluajina>
2024-01-18 20:23:14 +0100

    All states via SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,28 +1,31 @@
-"""Start link class to table in database
+#!/usr/bin/python3
 """
-import sys
-import sqlalchemy
-from model_state import Base, State
+Script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
 from sqlalchemy import create_engine
-from sqlalchemy.sql import select
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
 
 if __name__ == "__main__":
+    # Connection parameters
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
 
-    dialect = "mysql"
-    user = sys.argv[1]
-    passwd = sys.argv[2]
-    db = sys.argv[3]
-    host = "localhost"
-    port = 3306
+    # Database connection
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(username, password, db_name),
+                           pool_pre_ping=True)
 
-    engine = create_engine("{}://{}:{}@{}:{}/{}".format(
-        dialect,
-        user,
-        passwd,
-        host,
-        port,
-        db
-    ))
-    result = engine.execute("SELECT * FROM states ORDER BY id")
-    for _r in result:
-        print("{}: {}".format(_r[0], _r[1]))
+    # Create a configured "Session" class
+    Session = sessionmaker(bind=engine)
+
+    # Create a Session
+    session = Session()
+
+    # Query and print all State objects from the database
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
