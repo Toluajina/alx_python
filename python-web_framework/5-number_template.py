d42d87409bd2f30950ce8ee07c72744415301ea6
Toluajina <Toluajina>
2024-01-25 22:10:28 +0100

    Number template

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    html_content = '<!DOCTYPE html>\n<html lang="en">\n<head>\n<meta charset="UTF-8">\n<meta name="viewport" content="width=device-width, initial-scale=1.0">\n<title>Number: {{ n }}</title>\n</head>\n<body>\n<h1>Number: {{ n }}</h1>\n</body>\n</html>'
-    return render_template_string(html_content, n=n)
+    return render_template('templates/5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
