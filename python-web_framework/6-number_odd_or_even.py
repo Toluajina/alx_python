1d4be594e214999b356047c93ec13bce7b0107c1
Toluajina <Toluajina>
2024-01-25 21:43:04 +0100

    Odd or even?

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -22,42 +22,36 @@ Usage:
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
-    """Route: /"""
     return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)
-def display_hbnb():
-    """Route: /hbnb"""
+@app.route('/hbnb')
+def hbnb():
     return 'HBNB'
 
-@app.route('/c/<text>', strict_slashes=False)
-def display_c(text):
-    """Route: /c/<text>"""
-    return 'C ' + text.replace('_', ' ')
+@app.route('/c/<text>')
+def c(text):
+    return 'C {}'.format(text.replace('_', ' '))
 
-@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def display_python(text):
-    """Route: /python/<text>"""
-    return 'Python ' + text.replace('_', ' ')
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    return 'Python {}'.format(text.replace('_', ' '))
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def display_number(n):
-    """Route: /number/<n>"""
+@app.route('/number/<int:n>')
+def number(n):
     return '{} is a number'.format(n)
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_template/<int:n>')
 def number_template(n):
-    """Route: /number_template/<n>"""
-    return render_template('5-number.html', n=n)
+    return render_template('5-number.html', number=n)
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>')
 def number_odd_or_even(n):
-    """Route: /number_odd_or_even/<n>"""
-    return render_template('6-number_odd_or_even.html', n=n)
+    return render_template('6-number_odd_or_even.html', number=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
