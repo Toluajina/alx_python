60f6e6d159d6fd7e78b90ed2ca860f9ff2b9f05e
Toluajina <Toluajina>
2024-01-25 07:59:16 +0100

    Hello Flask!

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,9 +1,16 @@
+"""
+Simple Flask web application.
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Route that displays "Hello HBNB!".
+    """
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
