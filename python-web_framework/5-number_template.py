8990b34416237a3fdb94bd7077eac29e8445ac1d
Toluajina <Toluajina>
2024-01-25 21:05:02 +0100

    Number template

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -52,6 +52,3 @@ def display_number_template(n):
     H1 tag: "Number: n" inside the tag BODY.
     """
     return render_template('5-number.html', n=n)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
