3a7f2622927f184c48c45ee654bb6a116a9e44db
Toluajina <Toluajina>
2024-02-29 23:09:33 +0100

    Gather data from an API

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,32 @@
+import sys
+import requests
+
+
+def get_employee_info(employee_id):
+    # Retrieve employee details
+    employee_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    employee_data = employee_response.json()
+    employee_name = employee_data['name']
+
+    # Retrieve employee's todo list
+    todo_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos")
+    todo_data = todo_response.json()
+
+    # Calculate completed tasks
+    completed_tasks = [task for task in todo_data if task['completed']]
+    num_completed_tasks = len(completed_tasks)
+    total_tasks = len(todo_data)
+
+    # Print employee todo list progress
+    print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t{task['title']}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 script_name.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_info(employee_id)
