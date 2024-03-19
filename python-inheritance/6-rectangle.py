7e86681cf528405f2f4fd7e85619381cbdfdf94a
Toluajina <Toluajina>
2024-03-19 16:22:09 +0100

    Rectangle

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,5 +1,5 @@
 """Rectangle class Module"""
-BaseGeometry = __import__("7-base_geometry").BaseGeometry
+BaseGeometry = __import__("5-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
