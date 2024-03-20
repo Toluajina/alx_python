30443a5348c67d84ddfb89edef48888690e285d3
Toluajina <Toluajina>
2024-03-20 17:54:55 +0100

    Dictionary of list of dictionaries

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,17 +1,36 @@
 """Exports to-do list information of all employees to JSON format."""
-import json
 import requests
+import json
+
+def export_todo_all_employees():
+    url_users = "https://jsonplaceholder.typicode.com/users"
+    users_response = requests.get(url_users)
+    if users_response.status_code != 200:
+        print("Failed to retrieve user data.")
+        return
+    
+    users_data = users_response.json()
+    todo_data = {}
+
+    for user in users_data:
+        user_id = user['id']
+        username = user['username']
+        todos_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}/todos")
+        if todos_response.status_code != 200:
+            print(f"Failed to retrieve todos for user with ID {user_id}.")
+            continue
+        
+        todos = todos_response.json()
+        user_todos = []
+        for todo in todos:
+            task_title = todo['title']
+            completed = todo['completed']
+            user_todos.append({"username": username, "task": task_title, "completed": completed})
+        
+        todo_data[user_id] = user_todos
+    
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump(todo_data, jsonfile)
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    users = requests.get(url + "users").json()
-
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump({
-            u.get("id"): [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": u.get("username")
-            } for t in requests.get(url + "todos",
-                                    params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+    export_todo_all_employees()
