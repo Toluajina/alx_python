365f81560383a07547cab311a6d2bb6ebfccb1ec
Toluajina <Toluajina>
2024-03-20 18:04:27 +0100

    Dictionary of list of dictionaries

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -2,41 +2,16 @@
 import json
 import requests
 
-def export_todo_all_employees():
-    url_users = "https://jsonplaceholder.typicode.com/users"
-    users_response = requests.get(url_users)
-    if users_response.status_code != 200:
-        print("Failed to retrieve user data.")
-        return
-    
-    users_data = users_response.json()
-    todo_data = {}
-
-    for user in users_data:
-        user_id = user['id']
-        username = user['username']
-        todos_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{user_id}/todos")
-        if todos_response.status_code != 200:
-            print(f"Failed to retrieve todos for user with ID {user_id}.")
-            continue
-        
-        todos = todos_response.json()
-        user_todos = []
-        for todo in todos:
-            task_title = todo['title']
-            completed = todo['completed']
-            user_todos.append({"username": username, "task": task_title, "completed": completed})
-        
-        todo_data[user_id] = user_todos
-    
-    with open("todo_all_employees.json", "w") as jsonfile:
-        json.dump(todo_data, jsonfile)
-    
-    # Check if all users exist in output
-    if len(todo_data) == len(users_data):
-        print("All users found: OK")
-    else:
-        print("Some users are missing from the output.")
-
 if __name__ == "__main__":
-    export_todo_all_employees()
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users").json()
+
+    with open("todo_all_employees.json", "w") as jsonfile:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in users}, jsonfile)
